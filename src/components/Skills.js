c74d86041d69f2822075660caf26899200b1a2b0
bryant-jimenez <eevee@stanford.edu>
2024-09-28 02:17:36 -0600

    updated carousel, fixed buggy dragging + added vue to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,8 +16,7 @@ import unixLogo from "../assets/img/terminal-logo.png";
 import sqlLogo from "../assets/img/sql-logo.svg";
 import swiftLogo from "../assets/img/swiftLogo.svg"
 import goLogo from "../assets/img/go-logo.png";
-import bigqueryLogo from "../assets/img/bigquery-logo.png";
-import matlabLogo from "../assets/img/matlab-logo.png";
+import vueLogo from "../assets/img/vue.png";
 import darkspace from "../assets/img/darkspace.jpg"
 
 
@@ -29,11 +28,11 @@ export const Skills = () => {
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 3
+          items: 4
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 2
+          items: 3
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
@@ -53,66 +52,71 @@ export const Skills = () => {
                             <p>
                                 Programming Languages/Frameworks
                             </p>
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <Carousel responsive={responsive}  
+                                infinite={true} 
+                                className="skill-slider"
+                                slidesToSlide={1} 
+                                containerClass="carouselButtons"
+                                centerMode={true}>
                                 <div className="item">
-                                    <img src={pythonLogo} alt=""/>
-                                    <h4>Python</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={goLogo} alt=""/>
-                                    <h4>GoLang</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={cppLogo} alt=""/>
-                                    <h4>C++</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={cLogo} alt=""/>
-                                    <h4>C</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={swiftLogo} alt=""/>
-                                    <h4>Swift</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={jsLogo} alt=""/>
+                                    <img src={jsLogo} draggable={false} alt=""/>
                                     <h4>JavaScript</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={typescriptLogo} alt=""/>
-                                    <h4>TypeScript</h4>
+                                    <img src={goLogo} draggable={false} alt=""/>
+                                    <h4>GoLang</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={cssLogo} alt=""/>
-                                    <h4>CSS</h4>
+                                    <img src={pythonLogo} draggable={false} alt=""/>
+                                    <h4>Python</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={htmlLogo} alt=""/>
-                                    <h4>HTML</h4>
-                                </div>
-                                <div className="item">
-                                    <img src={reactLogo} alt=""/>
+                                    <img src={reactLogo} draggable={false} alt=""/>
                                     <h4>React</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={nextLogo} alt=""/>
+                                    <img src={cLogo} draggable={false} alt=""/>
+                                    <h4>C</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={cppLogo} draggable={false} alt=""/>
+                                    <h4>C++</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={swiftLogo} draggable={false} alt=""/>
+                                    <h4>Swift</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={typescriptLogo} draggable={false} alt=""/>
+                                    <h4>TypeScript</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={cssLogo} draggable={false} alt=""/>
+                                    <h4>CSS</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={htmlLogo} draggable={false} alt=""/>
+                                    <h4>HTML</h4>
+                                </div>
+                                <div className="item">
+                                    <img src={nextLogo} draggable={false} alt=""/>
                                     <h4>Next.js</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={tailwindLogo} alt=""/>
+                                    <img src={tailwindLogo} draggable={false} alt=""/>
                                     <h4>TailwindCSS</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={unixLogo} alt=""/>
+                                    <img src={unixLogo} draggable={false} alt=""/>
                                     <h4>Unix</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={sqlLogo} alt=""/>
+                                    <img src={sqlLogo} draggable={false} alt=""/>
                                     <h4>SQL</h4>
                                 </div>
                                 <div className="item">
-                                    <img src={bigqueryLogo} alt=""/>
-                                    <h4>Google BigQuery</h4>
+                                    <img src={vueLogo} draggable={false} alt=""/>
+                                    <h4>Vue</h4>
                                 </div>
                             </Carousel>
                         </div>
